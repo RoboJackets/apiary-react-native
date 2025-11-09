e4203ca33ddf923365d91e435cf1b987cda8f744
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2025-11-09 13:12:28 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -13,6 +13,8 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
+const unused = 0;
+
 function AuthProvider({ children }: AuthProviderProps) {
   const [authenticated, setAuthenticated] = useState(false);
   return (
