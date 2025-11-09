6d9f239b4bb46d0e17ca8cccf6f69ef13412f351
jvogt23 <jamesvogt70@gmail.com>
2025-11-09 12:05:47 -0500

    Configured linter, fixed existing problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,52 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:prettier/recommended', // Prettier integration
+  ],
+  env: {
+    es2022: true,
+    node: true,
+    browser: true,
+    'react-native/react-native': true,
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    // TypeScript rules
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    // React rules
+    'react/prop-types': 'off', // TS handles props types
+
+    // React Native rules
+    'react-native/no-inline-styles': 'warn',
+    'react-native/split-platform-components': 'warn',
+    'react-native/no-color-literals': 'off',
+
+    // Prettier formatting rules
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        printWidth: 100,
+        tabWidth: 2,
+        semi: true,
+      },
+    ],
+  },
 };
--- Attendance/AttendanceScreen.tsx
+++ Attendance/AttendanceScreen.tsx
@@ -1,17 +1,13 @@
-import { ParamListBase, useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
 import { Text, View } from 'react-native';
 
 function AttendanceScreen() {
-
-    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
-
-    return (
+  return (
+    // eslint-disable-next-line react-native/no-inline-styles
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Attendance Screen</Text>
+      <Text>Attendance Screen</Text>
     </View>
-    );
+  );
 }
 
 export default AttendanceScreen;
