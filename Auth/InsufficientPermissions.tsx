4fcb23ba05f6573f005ba95534b649690e31d2dd
elysseaa <elysseaa@gmail.com>
2026-01-18 20:45:25 -0500

    Fix inline styles, permission handling
    - Moved all inline styles to stylesheet
    - Fixed permission handling to render satisfied permissions as complete
    - Updated icon/button colors to use Colors.ts colors
    - Ran linter to reduce tab size

--- Auth/InsufficientPermissions.tsx
+++ Auth/InsufficientPermissions.tsx
@@ -1,104 +1,157 @@
 import MaterialIcons from '@react-native-vector-icons/material-icons';
 import React, { useState } from 'react';
 import { Button, Modal, StyleSheet, Text, View } from 'react-native';
+import { Colors } from '../Themes/Colors';
 
-type InsufficientPermissionsProps = { 
-    featureName: string,
-    onRetry: () => void,
-    missingPermissions: string[],
-    requiredPermissions: string[],
+type InsufficientPermissionsProps = {
+  featureName: string;
+  onRetry: () => void;
+  missingPermissions: string[];
+  requiredPermissions: string[];
 };
 
-function InsufficientPermissions({ featureName, onRetry, 
-    missingPermissions, requiredPermissions }: InsufficientPermissionsProps) {
-    const [detailsVisible, setDetailsVisible] = useState(false);
+function InsufficientPermissions({
+  featureName,
+  onRetry,
+  missingPermissions,
+  requiredPermissions,
+}: InsufficientPermissionsProps) {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+  const satisfiedPermissions = requiredPermissions.filter(
+    (item) => !missingPermissions.includes(item),
+  );
 
-    type PermissionDetailsListProps = { 
-        permissions: string[],
-        hasPermission: boolean
-    };
-    const PermissionDetailsList = ({permissions, hasPermission} : PermissionDetailsListProps) => {
-        return permissions.map((permission, index) => (
-            <View key={index} style={[ styles.permissionItem, { borderTopWidth: index === 0 ? 1 : 0 }]}>
-                {hasPermission? 
-                    <MaterialIcons name={"check-circle-outline"} size={30} color="#36B92B" style={{ margin: 20 }} />
-                    :
-                    <MaterialIcons name={"error-outline"} size={30} color="#b00020" style={{ margin: 20 }} />
-                }
-                <Text style={{ fontSize: 20 }}>{permission}</Text>
-            </View>
-        ));
-    }
+  type PermissionDetailsListProps = {
+    permissions: string[];
+    hasPermission: boolean;
+  };
+  const PermissionDetailsList = ({ permissions, hasPermission }: PermissionDetailsListProps) => {
+    return permissions.map((permission, index) => (
+      <View key={index} style={index === 0 ? styles.permissionItemFirst : styles.permissionItem}>
+        {hasPermission ? (
+          <MaterialIcons
+            name={'check-circle-outline'}
+            size={30}
+            color={Colors.success}
+            style={styles.icon}
+          />
+        ) : (
+          <MaterialIcons
+            name={'error-outline'}
+            size={30}
+            color={Colors.danger}
+            style={styles.icon}
+          />
+        )}
+        <Text style={styles.permissionItemText}>{permission}</Text>
+      </View>
+    ));
+  };
 
-    const PermissionDetailsDialog = () => {
-        return (
-            <Modal
-            animationType="fade"
-            transparent={true}>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <View style={styles.modalView}>
-                        <Text style={{ fontSize: 25, marginVertical: 20 }}>Required Permissions</Text>
-                        <PermissionDetailsList permissions={missingPermissions} hasPermission={false}/>
-                        <PermissionDetailsList permissions={requiredPermissions} hasPermission={true}/>
-                        <View style={{ alignItems:'flex-end', marginVertical: 20 }}>
-                            <Button
-                                onPress={() => setDetailsVisible(false)}
-                                title="Close"
-                            />
-                        </View>
-                    </View>
-                </View>
-            </Modal>
-        );
-    };
-    
+  const PermissionDetailsDialog = () => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', margin: 10 }}>
-            {
-                detailsVisible && 
-                <PermissionDetailsDialog></PermissionDetailsDialog>
-            }
-            <MaterialIcons name={"error-outline"} size={100} color="#b00020" style={{ padding: 10 }} />
-            <Text style={{ fontSize: 30, textAlign: "center", marginBottom: 20 }}>
-                {featureName} permissions required
-            </Text>
-            <View style={{display:"flex", flexDirection:"row", flexWrap:"wrap"}}>
-                <View style={{marginHorizontal: 5}}>
-                    <Button onPress={() => {}} title="Go to #it-helpdesk" />
-                </View>
-                <View style={{marginHorizontal: 5}}>
-                    <Button onPress={onRetry} color="#EEB211" title="Retry" />
-                </View>
+      <Modal animationType="fade" transparent={true}>
+        <View style={styles.viewContainer}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Required Permissions</Text>
+            <PermissionDetailsList permissions={missingPermissions} hasPermission={false} />
+            <PermissionDetailsList permissions={satisfiedPermissions} hasPermission={true} />
+            <View style={styles.modalButton}>
+              <Button onPress={() => setDetailsVisible(false)} title="Close" />
             </View>
-            <View style={{marginTop: 10}}>
-                <Button onPress={() => setDetailsVisible(true)} title="View details" />
-            </View>
-        </View> 
+          </View>
+        </View>
+      </Modal>
     );
+  };
+
+  return (
+    <View style={styles.viewContainer}>
+      {detailsVisible && <PermissionDetailsDialog></PermissionDetailsDialog>}
+      <MaterialIcons name={'error-outline'} size={100} color={Colors.danger} style={styles.icon} />
+      <Text style={styles.mainText}>{featureName} permissions required</Text>
+      <View style={styles.buttonTopRow}>
+        <View style={styles.button}>
+          <Button onPress={() => {}} title="Go to #it-helpdesk" />
+        </View>
+        <View style={styles.button}>
+          <Button onPress={onRetry} color={Colors.Gold} title="Retry" />
+        </View>
+      </View>
+      <View style={styles.buttonBottomRow}>
+        <Button onPress={() => setDetailsVisible(true)} title="View details" />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    modalView: {
-        margin: 30,
-        backgroundColor: 'white',
-        borderRadius: 20,
-        padding: 20,
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-        width:"75%"
+  button: {
+    marginHorizontal: 5,
+  },
+  buttonBottomRow: {
+    marginTop: 10,
+  },
+  buttonTopRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  icon: {
+    margin: 10,
+  },
+  mainText: {
+    fontSize: 30,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  modalButton: {
+    alignItems: 'flex-end',
+    marginVertical: 20,
+  },
+  modalText: {
+    fontSize: 20,
+    marginVertical: 20,
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    elevation: 5,
+    margin: 20,
+    padding: 20,
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    permissionItem: { 
-        justifyContent: 'flex-start', 
-        alignItems: "center", 
-        flexDirection:"row", 
-        borderBlockColor:"gray", 
-        borderBottomWidth: 1
-    }
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    width: '75%',
+  },
+  permissionItem: {
+    alignItems: 'center',
+    borderBlockColor: 'gray',
+    borderBottomWidth: 1,
+    borderTopWidth: 0,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  permissionItemFirst: {
+    alignItems: 'center',
+    borderBlockColor: 'gray',
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  permissionItemText: {
+    fontSize: 15,
+  },
+  viewContainer: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    margin: 10,
+  },
 });
 
 export default InsufficientPermissions;
